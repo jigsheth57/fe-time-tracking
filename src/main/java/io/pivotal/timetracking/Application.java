934ed475423a34b523c5eff3bfafaf998edf0b91
Jignesh Sheth <jsheth@pivotal.io>
2016-02-29 17:27:09 -0600

    updates

--- src/main/java/io/pivotal/timetracking/Application.java
+++ src/main/java/io/pivotal/timetracking/Application.java
@@ -3,7 +3,6 @@ package io.pivotal.timetracking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 
 /**
@@ -15,9 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 
 @SpringBootApplication
-@ComponentScan
 @EnableAutoConfiguration
-//@EnableDiscoveryClient
 public class Application {
 
 	/**
