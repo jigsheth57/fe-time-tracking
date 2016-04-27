5d646f36dfc9ccc26e5829ef619c0dd9ddd80b00
Jignesh Sheth <jsheth@pivotal.io>
2016-04-27 17:21:35 -0500

    update

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,2 @@
-drop table if exists time_entry
 create table time_entry (time_entry_id bigint not null auto_increment, account_name varchar(255) not null, date date not null, fe_name varchar(255) not null, hours double precision not null, primary key (time_entry_id))
 	
