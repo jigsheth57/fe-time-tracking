48bc63df8caaaa6f158fe83f90191f36050a58a4
Jig Sheth <jsheth@pivotal.io>
2019-06-15 14:24:55 -0500

    update

--- ~/Downloads/test/fe-time-tracking-1.0.0-SNAPSHOT/org/springframework/boot/loader/JavaAgentDetector.java
+++
@@ -1,7 +0,0 @@
-package org.springframework.boot.loader;
-
-import java.net.URL;
-
-public interface JavaAgentDetector {
-   boolean isJavaAgentJar(URL var1);
-}
--- ~/Downloads/test/fe-time-tracking-1.0.0-SNAPSHOT/org/springframework/boot/loader/jar/JarEntryFilter.java
+++
@@ -1,7 +0,0 @@
-package org.springframework.boot.loader.jar;
-
-import org.springframework.boot.loader.util.AsciiBytes;
-
-public interface JarEntryFilter {
-   AsciiBytes apply(AsciiBytes var1, JarEntryData var2);
-}
