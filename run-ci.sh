516048ed9907b12dc132c6e964637f451b085764
Jignesh Sheth <jigneshsheth@Jigs-Mac-Pro.local>
2021-06-09 17:27:46 -0500

    version v1.12

--- run-ci.sh
+++ run-ci.sh
@@ -3,6 +3,7 @@ set -o errexit
 
 git add .
 git commit -m "version $1"
-git push
 git tag -a $1 -m "version $1"
 git push origin --tags
+
+../argocd-manifests/fe-time-tracking/run-ci.sh fe-time-tracking $1
